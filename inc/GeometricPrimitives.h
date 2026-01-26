f2d706bf32c4b29e61594bef7e3d2ea96810715f
Josh Cohen <joshua_cohen@me.com>
2026-01-26 12:18:09 -0800

    More geometric primitives: ray, plane, sphere, aabb

--- inc/GeometricPrimitives.h
+++ inc/GeometricPrimitives.h
@@ -1,22 +1,46 @@
 #pragma once
 #include "Matrix.h"
 
-struct Vert2d {
-	Math3D::Vec2f pos;
-	Math3D::Vec3f col;
-	Math3D::Vec2f uv;
-};
+namespace Math3D {
+	using Point = Vec3f;
 
-struct Vert3d {
-	Math3D::Vec3f pos;
-	Math3D::Vec3f norm;
-	Math3D::Vec2f uv;
-};
+	struct Vert2d {
+		Vec2f pos;
+		Vec3f col;
+		Vec2f uv;
+	};
 
-struct Tri2d {
-	Vert2d verts[3];
-};
+	struct Vert3d {
+		Vec3f pos;
+		Vec3f norm;
+		Vec2f uv;
+	};
 
-struct Tri3d {
-	Vert3d verts[3];
-};
+	struct Tri2d {
+		Vert2d verts[3];
+	};
+
+	struct Tri3d {
+		Vert3d verts[3];
+	};
+
+	struct Ray {
+		Point point;
+		Vec3f dir;
+	};
+
+	struct Plane {
+		Vec3f normal;
+		float distance;
+	};
+
+	struct Sphere {
+		Point center;
+		float radius;
+	};
+
+	struct AABB {
+		Point center;
+		float halfwidths[3];
+	};
+}
